9753e568845bf2c6a6372a0e8ca4e49f492ef46c
Bruna Yokomizo <bruna@example.com>
2026-08-27 21:45:35 -0300

    Inclusão da tela Transparência

--- app/src/main/java/br/com/fiap/safework/ui/theme/Color.kt
+++ app/src/main/java/br/com/fiap/safework/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package br.com.fiap.safework.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightPrimary = Color(0xFF00A868)
+val LightSecondary = Color(0xFF3B82F6)
+val LightBackground = Color(0xFFF8FAFC)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF0F172A)
+val LightOnSurface = Color(0xFF1E293B)
+val LightTertiary = Color(0xFF64748B)
+val LightOnTertiary = Color(0xFFFFFFFF)
+
+
+val DarkPrimary = Color(0xFF10B981)
+val DarkSecondary = Color(0xFF60A5FA)
+val DarkBackground = Color(0xFF0F172A)
+val DarkSurface = Color(0xFF1E293B)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkOnBackground = Color(0xFFF8FAFC)
+val DarkOnSurface = Color(0xFFF8FAFC)
+val DarkTertiary = Color(0xFF94A3B8)
+val DarkOnTertiary = Color(0xFF000000)
+
+// ícones dos cards
+val LightBlueIconBg = Color(0xFFEFF6FF)
+val LightGreenIconBg = Color(0xFFECFDF5)
--- app/src/main/java/br/com/fiap/safework/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/safework/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package br.com.fiap.safework.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.safework.R
 
-// Set of Material typography styles to start with
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_regular, FontWeight.Medium),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 11.sp
     )
-    */
 )
